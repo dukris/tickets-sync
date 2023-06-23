632ac5b9ca3b57be8b419e2571ce105190beaa12
hizmailovich <theannalios@gmail.com>
2023-06-23 10:20:55 +0300

    Initial commit

---
+++ src/main/java/com/solvd/tickets/TicketsApplication.java
@@ -0,0 +1,13 @@
+package com.solvd.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsApplication.class, args);
+	}
+
+}
