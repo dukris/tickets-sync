47bcab8290b5e78a8384350cfd9a9586e5fda7d0
hizmailovich <theannalios@gmail.com>
2023-07-02 15:25:25 +0300

    Dependencies for Jira

--- src/main/java/com/solvd/tickets/TicketsApplication.java
+++ src/main/java/com/solvd/tickets/TicketsApplication.java
@@ -2,10 +2,20 @@ package com.solvd.tickets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+/**
+ * Application.
+ */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TicketsApplication {
 
+	/**
+	 * Application start.
+	 *
+	 * @param args String[]
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TicketsApplication.class, args);
 	}
